7fd78103f6949e586bbf22cd0051dd62107957de
Agus sanjaya <agus.16092000@gmail.com>
2023-11-01 15:00:17 +0700

    first commit

---
+++ unit1.pas
@@ -0,0 +1,52 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    cbJabatan: TComboBox;
+    edtNama: TEdit;
+    edtGapok: TEdit;
+    edtTunjangan: TEdit;
+    Edit6: TEdit;
+    GroupBox1: TGroupBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Tetap: TRadioButton;
+    Honorer: TRadioButton;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
